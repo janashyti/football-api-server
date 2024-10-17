aad442fd903c18ef95958cf5674896fc680082e6
Jana Shyti <jshyti@eagles.bridgewater.edu>
2024-10-17 19:53:27 -0400

    new

---
+++ src/app.js
@@ -0,0 +1,25 @@
+require('dotenv').config({ debug: true });
+
+const express = require('express') 
+require('./database/mongoose')
+
+const cors = require('cors'); 
+const userRouter = require('./routers/studentuser')
+
+const app = express() 
+
+app.use(cors()) 
+app.use(function (req, res, next) { 
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); 
+    next(); 
+}); 
+  
+app.use(express.json()) 
+app.use(userRouter)
+
+
+const port = process.env.PORT || 3010 
+app.listen(port, () => { 
+    console.log('Server is up on port ' + port) 
+})
